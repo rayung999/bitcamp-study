b69bcc721c8372909efa7687243564bebb2d4dbe
BOOK-153M5AROP9\kimra <kimrawun@naver.com>
2022-07-19 17:09:19 +0900

    '.'

--- java-lang/app/src/main/java/Exam03.java
+++
@@ -1,5 +0,0 @@
-public class Exam03 {
-  public static void main(String[] args) { //entry point
-    System.out.println("Hello!");
-  }
-}
--- java-lang/app/src/main/java/Exam06.java
+++
@@ -1,8 +0,0 @@
-public class Exam06 {
-    public static void main(String[] args) {
-        System.out.println(true);       // boolean 값은 무조건 소문자
-        System.out.println(false);
-        System.out.println(10 == 11);   // false를 return한다
-        System.out.println(10 < 11);    // true를 return한다
-    }
-}
