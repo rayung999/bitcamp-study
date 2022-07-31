cf9279962f97a5911ab6702de619f15e2a214f8e
BOOK-153M5AROP9\kimra <kimrawun@naver.com>
2022-07-31 20:10:14 +0900

    '.'

--- java-lang/app/src/main/java/com/eomcs/exception/ex5/Exam0112.java
+++ java-lang/app/src/main/java/com/eomcs/exception/ex5/Exam0112.java
@@ -1,18 +1,13 @@
-// 예외 처리 전 - 2) finally {} 를 이용하여 자원 해제를 자동화 하기
+// 예외 처리 전 - 3) try-with-resources 를 이용하여 자원 해제를 자동화 하기
 package com.eomcs.exception.ex5;
 
 import java.sql.Date;
 import java.util.Scanner;
 
-public class Exam0111 {
+public class Exam0112 {
 
   static Board read() {
-
-    Scanner keyScan = null;
-
-    try {
-      keyScan = new Scanner(System.in);
-
+    try (Scanner keyScan = new Scanner(System.in)) {
       Board board = new Board();
 
       System.out.print("번호> ");
@@ -28,12 +23,7 @@ public class Exam0111 {
       board.setCreatedDate(Date.valueOf(keyScan.nextLine()));
 
       return board;
-
-    } finally { 
-      // 정상적으로 실행하든 예외가 발생하든지 간에 무조건 close()를 실행한다.
-      keyScan.close();
-      System.out.println("Scanner 자원을 해제시켰다.");
-    }
+    } 
   }
 
   public static void main(String[] args) {
