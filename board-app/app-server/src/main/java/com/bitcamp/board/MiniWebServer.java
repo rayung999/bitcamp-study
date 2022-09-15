3e072900856797e9ad77474b2a7fc5680ca62487
rayung999 <kimrawun@naver.com>
2022-09-15 14:21:39 +0900

    add ErrorHandler

--- board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
@@ -2,7 +2,12 @@ package com.bitcamp.board;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import com.bitcamp.board.handler.ErrorHandler;
+import com.bitcamp.board.handler.WelcomeHandler;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -17,19 +22,36 @@ public class MiniWebServer {
       public void handle(HttpExchange exchange) throws IOException {
         System.out.println("클라이언트가 요청함!");
 
-        String response = "ABCabc123가각간";
-        byte[] bytes = response.getBytes("UTF-8");
+        URI requestUri = exchange.getRequestURI();
+
+        String path = requestUri.getPath();
+
+        WelcomeHandler welcomeHandler = new WelcomeHandler();
+        ErrorHandler errorHandler = new ErrorHandler();
+
+        byte[] bytes = null;
+
+        try (StringWriter stringWriter = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(stringWriter)) {
+
+          if (path.equals("/")) {
+            welcomeHandler.service(printWriter);
+          } else {
+            errorHandler.error(printWriter);
+          }
+
+          bytes = stringWriter.toString().getBytes("UTF-8");
+        }
 
         // 보내는 콘텐트의 MIME 타입이 무엇인지 응답 헤더에 추가한다.
         Headers responseHeaders = exchange.getResponseHeaders();
-        responseHeaders.add("Content-type", "text/plain; charset=UTF-8");
+        responseHeaders.add("Content-Type", "text/html; charset=UTF-8");
 
         exchange.sendResponseHeaders(200, bytes.length);
 
         OutputStream out = exchange.getResponseBody();
         out.write(bytes);
         out.close();
-
       }
     }
 
@@ -37,6 +59,8 @@ public class MiniWebServer {
     server.createContext("/", new MyHttpHandler());
     server.setExecutor(null);
     server.start();
+
     System.out.println("서버 시작!");
   }
+
 }
