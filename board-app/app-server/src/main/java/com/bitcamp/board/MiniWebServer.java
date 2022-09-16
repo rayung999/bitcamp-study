599eed8bb86c1c07896b9cf54181d7cd31740f8a
rayung999 <kimrawun@naver.com>
2022-09-16 11:56:00 +0900

    URL 인코딩

--- board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
@@ -6,12 +6,15 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.net.URLDecoder;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.HashMap;
 import java.util.Map;
+import com.bitcamp.board.dao.BoardDao;
 import com.bitcamp.board.dao.MariaDBBoardDao;
 import com.bitcamp.board.dao.MariaDBMemberDao;
+import com.bitcamp.board.dao.MemberDao;
 import com.bitcamp.board.handler.BoardHandler;
 import com.bitcamp.board.handler.ErrorHandler;
 import com.bitcamp.board.handler.WelcomeHandler;
@@ -20,89 +23,103 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-// 1) 기본 서버 만들기
-// 2) 한글 콘텐트 응답하기
-// 3) HTML 콘텐트 출력하기
+// 1) 기본 웹 서버 만들기
+// 2) 한글 콘텐트를 출력하기
+// 3) HTML 콘텐트를 출력하기
 // 4) 메인 화면을 출력하는 요청처리 객체를 분리하기
 // 5) 요청 자원의 경로를 구분하여 처리하기
-// 6) 요청 자원을 처리하는 객체의 사용 규칙을 통일한다.
+// 6) 게시글 요청 처리하기
+//
 public class MiniWebServer {
-  public static void main(String[] args) throws Exception {
-    Connection con =
-        DriverManager.getConnection("jdbc:mariadb://localhost:3306/studydb", "study", "1111");
 
-    MariaDBBoardDao boardDao = new MariaDBBoardDao(con);
-    MariaDBMemberDao memberDao = new MariaDBMemberDao(con);
+  public static void main(String[] args) throws Exception {
+    Connection con = DriverManager.getConnection(
+        "jdbc:mariadb://localhost:3306/studydb","study","1111");
+
+    BoardDao boardDao = new MariaDBBoardDao(con);
+    MemberDao memberDao = new MariaDBMemberDao(con);
 
     WelcomeHandler welcomeHandler = new WelcomeHandler();
     ErrorHandler errorHandler = new ErrorHandler();
     BoardHandler boardHandler = new BoardHandler(boardDao);
 
-    // 클라이언트 요청이 들어 왔을 때 호출되는 객체
     class MyHttpHandler implements HttpHandler {
       @Override
       public void handle(HttpExchange exchange) throws IOException {
-        // 클라이언트 요청이 들어왔을 때마다 호출된다.
         System.out.println("클라이언트가 요청함!");
 
         URI requestUri = exchange.getRequestURI();
-
-        String path = requestUri.getPath();
-        String query = requestUri.getQuery();
-
+        String path = requestUri.getPath(); 
+        //        String query = requestUri.getQuery();// 디코딩을 제대로 수행하지 못한다!
+        String query = requestUri.getRawQuery(); // 디코딩 없이 query string을 그대로 리턴받기
         byte[] bytes = null;
 
         try (StringWriter stringWriter = new StringWriter();
             PrintWriter printWriter = new PrintWriter(stringWriter)) {
 
-          Map<String, String> paramMap = new HashMap<>();
+          Map<String,String> paramMap = new HashMap<>();
           if (query != null && query.length() > 0) { // 예) no=1&title=aaaa&content=bbb
             String[] entries = query.split("&");
             for (String entry : entries) { // 예) no=1
               String[] kv = entry.split("=");
-              paramMap.put(kv[0], kv[1]);
+              // 웹브라우저가 보낸 파라미터 값은 저장하기 전에 URL 디코딩 한다.
+              paramMap.put(kv[0], URLDecoder.decode(kv[1], "UTF-8"));
             }
           }
+          System.out.println(requestUri);
+          System.out.println(query);
           System.out.println(paramMap);
 
           if (path.equals("/")) {
             welcomeHandler.service(paramMap, printWriter);
+
           } else if (path.equals("/board/list")) {
             boardHandler.list(paramMap, printWriter);
+
           } else if (path.equals("/board/detail")) {
             boardHandler.detail(paramMap, printWriter);
+
           } else if (path.equals("/board/update")) {
             boardHandler.update(paramMap, printWriter);
+
           } else if (path.equals("/board/delete")) {
             boardHandler.delete(paramMap, printWriter);
+
+          } else if (path.equals("/board/form")) {
+            boardHandler.form(paramMap, printWriter);
+
+          } else if (path.equals("/board/add")) {
+            boardHandler.add(paramMap, printWriter);
+
           } else {
             errorHandler.error(paramMap, printWriter);
           }
+
           bytes = stringWriter.toString().getBytes("UTF-8");
+
         } catch (Exception e) {
-          bytes = "요청 처리 중 오류 발생".getBytes("UTF-8"); // 오류 내용을 클라이언트에게 보낸다.
+          bytes = "요청 처리 중 오류 발생!".getBytes("UTF-8");
           e.printStackTrace(); // 서버 콘솔 창에 오류에 대한 자세한 내용을 출력한다.
         }
 
         // 보내는 콘텐트의 MIME 타입이 무엇인지 응답 헤더에 추가한다.
         Headers responseHeaders = exchange.getResponseHeaders();
         responseHeaders.add("Content-Type", "text/html; charset=UTF-8");
-        // 응답 헤더 전송
-        exchange.sendResponseHeaders(200, bytes.length);
-        // 콘텐트 출력 스트림 준비
-        OutputStream out = exchange.getResponseBody();
 
+        exchange.sendResponseHeaders(200, bytes.length);
+
+        OutputStream out = exchange.getResponseBody();
         out.write(bytes);
         out.close();
       }
     }
+
     HttpServer server = HttpServer.create(new InetSocketAddress(8888), 0);
-    server.createContext("/", new MyHttpHandler());
-    server.setExecutor(null); // HttpServer에 기본으로 설정되어 있는 Executor 사용
-    // Executor? 멀티 스레딩을 수행하는 객체
-    server.start(); // HttpServer를 시작시킨 후 즉시 리턴한다.
+    server.createContext("/", new MyHttpHandler()); 
+    server.setExecutor(null); 
+    server.start();
+
     System.out.println("서버 시작!");
-    // main() 메서드 호출이 끝났다 하더라도
-    // 내부에서 생성한 스레드(예:HttpServer)가 종료되지 않으면 JVM도 종료되지 않는다.
   }
+
 }
--- board-app/app-server/src/main/java/com/bitcamp/board/handler/BoardHandler.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/handler/BoardHandler.java
@@ -1,13 +1,13 @@
+/*
+ * 게시글 메뉴 처리 클래스
+ */
 package com.bitcamp.board.handler;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 import com.bitcamp.board.dao.BoardDao;
 import com.bitcamp.board.domain.Board;
-import com.bitcamp.util.Prompt;
 
 public class BoardHandler {
 
@@ -17,30 +17,7 @@ public class BoardHandler {
     this.boardDao = boardDao;
   }
 
-  // // 템플릿 메서드 패턴(template method pattern):
-  // // - 수퍼클래스의 execute()에서 동작의 전체적인 흐름을 정의하고(틀을 만들고),
-  // // - 서브클래스의 service()에서 동작을 구체적으로 정의한다.(세부적인 항목을 구현)
-  // @Override
-  // public void service(int menuNo, DataInputStream in, DataOutputStream out) {
-  // try {
-  // switch (menuNo) {
-  // // case 0:
-  // // //핸들러를 종료할 때 breadcrumb 메뉴에 등록된 이 핸들러의 이름을 꺼낸다.
-  // // // App.breadcrumbMenu.pop();
-  // // return;
-  // case 1: this.onList(in, out); break;
-  // case 2: this.onDetail(in, out); break;
-  // case 3: this.onInput(in, out); break;
-  // case 4: this.onDelete(in, out); break;
-  // case 5: this.onUpdate(in, out); break;
-  // // default: System.out.println("메뉴 번호가 옳지 않습니다!");
-  // }
-  // } catch (Exception e) {
-  // throw new RuntimeException(e);
-  // }
-  // }
-
-  public void list(Map<String, String> paramMap, PrintWriter out) throws Exception {
+  public void list(Map<String,String> paramMap, PrintWriter out) throws Exception {
 
     out.println("<!DOCTYPE html>");
     out.println("<html>");
@@ -49,16 +26,16 @@ public class BoardHandler {
     out.println("<title>bitcamp</title>");
     out.println("<style>");
     out.println("tr:hover {");
-    out.println("  background-color: navy;");
+    out.println("  background-color: pink;");
     out.println("  color: white;");
     out.println("}");
     out.println("</style>");
     out.println("</head>");
-
     out.println("<body>");
     out.println("<h1>게시글</h1>");
-    out.println("<table border='1'>");
-    out.println("  <tr> ");
+    out.println("<a href='form'>새 글</a>");
+    out.println("<table border='10' bgcolor='yellow' bordercolor='black'>");
+    out.println("  <tr>");
     out.println("    <th>번호</th>");
     out.println("    <th>제목</th>");
     out.println("    <th>조회수</th>");
@@ -68,37 +45,36 @@ public class BoardHandler {
 
     List<Board> boards = boardDao.findAll();
     for (Board board : boards) {
-      // tempOut.printf("%d\t%s\t%d\t%d\t%s\n",
-      // board.no, board.title, board.viewCount, board.memberNo, board.createdDate);
-      out.println("  <tr> ");
-      out.printf("    <td>%d</td>", board.no);
-      out.printf("    <td><a href='/board/detail?no=%d'>%s</a></td>", board.no, board.title);
-      out.printf("    <td>%d</td>", board.viewCount);
-      out.printf("    <td>%d</td>", board.memberNo);
-      out.printf("    <td>%s</td>", board.createdDate);
-      out.println("  </tr>");
+      out.println("<tr>");
+      out.printf("  <td>%d</td>", board.no);
+      out.printf("  <td><a href='detail?no=%d'>%s</a></td>", board.no, board.title);
+      out.printf("  <td>%d</td>", board.viewCount);
+      out.printf("  <td>%d</td>", board.memberNo);
+      out.printf("  <td>%s</td>", board.createdDate);
+      out.println("</tr>");
     }
+
     out.println("</table>");
     out.println("</body>");
     out.println("</html>");
   }
 
-  public void detail(Map<String, String> paramMap, PrintWriter out) throws Exception {
+  public void detail(Map<String,String> paramMap, PrintWriter out) throws Exception {
+
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
     out.println("<meta charset=\"UTF-8\">");
     out.println("<title>bitcamp</title>");
     out.println("</head>");
-
     out.println("<body>");
-    out.println("<h1>게시글 상세정보</h1>");
+    out.println("<h1>게시글 상세 정보</h1>");
 
     int boardNo = Integer.parseInt(paramMap.get("no"));
     Board board = boardDao.findByNo(boardNo);
 
     if (board == null) {
-      out.println("<p>해당 번호의 게시글이 없습니다!</p>");
+      out.println("<p>해당 번호의 게시글이 없습니다.</p>");
 
     } else {
       out.println("<form action='update'>");
@@ -107,7 +83,7 @@ public class BoardHandler {
       out.printf("    <th>번호</th><td><input name='no' type='number' value='%d' readonly></td>", board.no);
       out.println("  </tr>");
       out.println("  <tr>");
-      out.printf("    <th>제목</th><td><input name='title' type='text' value='%s'></td>", board.title);
+      out.printf("    <th>제목</th><td><input name='title' type='text' value='%s' size='60'></td>", board.title);
       out.println("  </tr>");
       out.println("  <tr>");
       out.printf("    <th>내용</th><td><textarea name='content' rows='10' cols='60'>%s</textarea></td>", board.content);
@@ -122,59 +98,18 @@ public class BoardHandler {
       out.printf("    <th>등록일</th><td>%s</td>", board.createdDate);
       out.println("  </tr>");
       out.println("</table>");
-      out.println("<P>");
-      out.println("<button>변경</button>");
-      out.printf("<a href='delete?no=%d'>삭제</a>", board.no);
+      out.println("<p>");
+      out.println("  <button type='submit'>변경</button>");
+      out.printf("  <a href='delete?no=%d'>삭제</a>", board.no);
       out.println("</p>");
+      out.println("</form>");
     }
+
     out.println("</body>");
     out.println("</html>");
   }
-  // Prompt prompt = new Prompt(in, out);
-  //
-  // int boardNo = 0;
-  // while (true) {
-  // try {
-  // boardNo = prompt.inputInt("조회할 게시글 번호? ");
-  // break;
-  // } catch (Exception ex) {
-  // out.writeUTF("입력 값이 옳지 않습니다!");
-  // }
-  // }
-  // Board board = boardDao.findByNo(boardNo);
-  //
-  // try (StringWriter strOut = new StringWriter();
-  // PrintWriter tempOut = new PrintWriter(strOut);) {
-  //
-  // if (board == null) {
-  // tempOut.println("해당 번호의 게시글이 없습니다!");
-  // out.writeUTF(strOut.toString());
-  // return;
-  // }
-  // tempOut.printf("번호: %d\n", board.no);
-  // tempOut.printf("제목: %s\n", board.title);
-  // tempOut.printf("내용: %s\n", board.content);
-  // tempOut.printf("조회수: %d\n", board.viewCount);
-  // tempOut.printf("작성자: %d\n", board.memberNo);
-  // tempOut.printf("등록일: %s\n", board.createdDate);
-  // out.writeUTF(strOut.toString());
-  // }
-  // }
 
-
-  private void onInput(DataInputStream in, DataOutputStream out) throws Exception {
-    Prompt prompt = new Prompt(in, out);
-    Board board = new Board();
-
-    board.title = prompt.inputString("제목? ");
-    board.content = prompt.inputString("내용? ");
-    board.memberNo = prompt.inputInt("작성자? ");
-
-    boardDao.insert(board);
-    out.writeUTF("게시글을 등록했습니다.");
-  }
-
-  public void delete(Map<String, String> paramMap, PrintWriter out) throws Exception {
+  public void delete(Map<String,String> paramMap, PrintWriter out) throws Exception {
 
     out.println("<!DOCTYPE html>");
     out.println("<html>");
@@ -183,43 +118,24 @@ public class BoardHandler {
     out.println("<title>bitcamp</title>");
     out.println("<meta http-equiv='Refresh' content='3; url=list'>");
     out.println("</head>");
-
     out.println("<body>");
     out.println("<h1>게시글 삭제</h1>");
 
     int no = Integer.parseInt(paramMap.get("no"));
 
     if (boardDao.delete(no) == 0) {
-      out.println("<p>해당 번호의 게시글이 없습니다!</p>");
+      out.println("<p>해당 번호의 게시글이 없습니다.</p>");
 
     } else {
       out.println("<p>해당 게시글을 삭제했습니다.</p>");
-
     }
+
     out.println("</body>");
     out.println("</html>");
+
   }
 
-  //    Prompt prompt = new Prompt(in, out);
-  //    int boardNo = 0;
-  //    while (true) {
-  //      try {
-  //        boardNo = prompt.inputInt("삭제할 게시글 번호? ");
-  //        break;
-  //      } catch (Exception ex) {
-  //        out.writeUTF("입력 값이 옳지 않습니다!");
-  //      }
-  //    }
-  //
-  //    if (boardDao.delete(boardNo) == 1) {
-  //      out.writeUTF("삭제하였습니다.");
-  //    } else {
-  //      out.writeUTF("해당 번호의 게시글이 없습니다!");
-  //    }
-  //  }
-
-
-  public void update(Map<String, String> paramMap, PrintWriter out) throws Exception {
+  public void update(Map<String,String> paramMap, PrintWriter out) throws Exception {
 
     out.println("<!DOCTYPE html>");
     out.println("<html>");
@@ -228,7 +144,6 @@ public class BoardHandler {
     out.println("<title>bitcamp</title>");
     out.println("<meta http-equiv='Refresh' content='3; url=list'>");
     out.println("</head>");
-
     out.println("<body>");
     out.println("<h1>게시글 변경</h1>");
 
@@ -238,46 +153,82 @@ public class BoardHandler {
     board.content = paramMap.get("content");
 
     if (boardDao.update(board) == 0) {
-      out.println("<p>해당 번호의 게시글이 없습니다!</p>");
+      out.println("<p>해당 번호의 게시글이 없습니다.</p>");
 
     } else {
       out.println("<p>해당 게시글을 변경했습니다.</p>");
-
     }
+
     out.println("</body>");
     out.println("</html>");
   }
 
-  //    Prompt prompt = new Prompt(in, out);
-  //    int boardNo = 0;
-  //    while (true) {
-  //      try {
-  //        boardNo = prompt.inputInt("변경할 게시글 번호? ");
-  //        break;
-  //      } catch (Throwable ex) {
-  //        out.writeUTF("입력 값이 옳지 않습니다!");
-  //      }
-  //    }
-  //
-  //    Board board = boardDao.findByNo(boardNo);
-  //    if (board == null) {
-  //      out.writeUTF("해당 번호의 게시글이 없습니다!");
-  //      return;
-  //    }
-  //
-  //    board.title = prompt.inputString("제목?(" + board.title + ") ");
-  //    board.content = prompt.inputString(String.format("내용?(%s) ", board.content));
-  //
-  //    String input = prompt.inputString("변경하시겠습니까?(y/n) ");
-  //    if (input.equals("y")) {
-  //
-  //      if (boardDao.update(board) == 1) {
-  //        out.writeUTF("변경했습니다.");
-  //      } else {
-  //        out.writeUTF("변경 실패입니다!");
-  //      }
-  //    } else {
-  //      out.writeUTF("변경 취소했습니다.");
-  //    }
+  public void form(Map<String, String> paramMap, PrintWriter out) {
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset=\"UTF-8\">");
+    out.println("<title>bitcamp</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>게시글 입력</h1>");
+
+    out.println("<form action='add'>");
+    out.println("<table border='1'>");
+    out.println("  <tr>");
+    out.println("    <th>제목</th><td><input name='title' type='text' size='60'></td>");
+    out.println("  </tr>");
+    out.println("  <tr>");
+    out.println("    <th>내용</th><td><textarea name='content' rows='10' cols='60'></textarea></td>");
+    out.println("  </tr>");
+    out.println("  <tr>");
+    out.println("    <th>작성자</th><td><input name='writerNo' type='number' size='5'></td>");
+    out.println("  </tr>");
+    out.println("</table>");
+    out.println("<p>");
+    out.println("  <button type='submit'>등록</button>");
+    out.println("  <a href='list'>목록</a>");
+    out.println("</p>");
+    out.println("</form>");
+
+    out.println("</body>");
+    out.println("</html>");
+
+  }
+
+  public void add(Map<String,String> paramMap, PrintWriter out) throws Exception {
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset=\"UTF-8\">");
+    out.println("<title>bitcamp</title>");
+    out.println("<meta http-equiv='Refresh' content='3; url=list'>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>게시글 입력</h1>");
+
+    Board board = new Board();
+    board.title = paramMap.get("title");
+    board.content = paramMap.get("content");
+    board.memberNo = Integer.parseInt(paramMap.get("writerNo"));
+
+    if (boardDao.insert(board) == 0) {
+      out.println("<p>게시글을 등록할 수 없습니다!</p>");
+
+    } else {
+      out.println("<p>게시글을 등록했습니다.</p>");
+    }
+
+    out.println("</body>");
+    out.println("</html>");
+
+  }
+
 
 }
+
+
+
+
