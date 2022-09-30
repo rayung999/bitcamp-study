c11df1aff40e9ab56f2f6ed912b5ee2201406aac
rayung999 <kimrawun@naver.com>
2022-09-30 16:54:03 +0900

    '.'

--- board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardAddController.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardAddController.java
@@ -2,6 +2,7 @@ package com.bitcamp.board.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import javax.servlet.ServletException;
@@ -13,6 +14,7 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import com.bitcamp.board.dao.BoardDao;
+import com.bitcamp.board.domain.AttachedFile;
 import com.bitcamp.board.domain.Board;
 import com.bitcamp.board.domain.Member;
 
@@ -31,7 +33,7 @@ public class BoardAddController extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     try {
-      // multipart/form-data 형식으로 요청한 경우 
+      // multipart/form-data 형식으로 요청한 경우
       // 요청 데이터의 인코딩을 다음과 같이 지정할 수 없다.
       //      request.setCharacterEncoding("UTF-8");
 
@@ -41,8 +43,8 @@ public class BoardAddController extends HttpServlet {
       DiskFileItemFactory factory = new DiskFileItemFactory();
 
       // 2) 멀티파트 데이터를 분석할 객체 준비
-      // - 멀티파트 데이터를 파트 별로 쪼개서 이름과 데이터를 분리하여 
-      //   DiskFileItemFactory를 이용하여 임시 폴더에 저장한다. 
+      // - 멀티파트 데이터를 파트 별로 쪼개서 이름과 데이터를 분리하여
+      //   DiskFileItemFactory를 사용하여 임시 폴더에 저장한다.
       ServletFileUpload upload = new ServletFileUpload(factory);
 
       // 3) HttpServletRequest 객체를 통해 데이터를 읽어서 멀티파트 데이터를 처리한다.
@@ -51,9 +53,12 @@ public class BoardAddController extends HttpServlet {
       // - 물론 업로드 된 파일의 데이터는 DiskFileItemFactory에서 관리하고 있다.
       List<FileItem> items = upload.parseRequest(request);
 
-
       // 클라이언트가 멀티파트로 보낸 데이터를 저장할 도메인 객체를 준비한다.
       Board board = new Board();
+
+      // 첨부파일명을 저장할 컬렉션 객체 준비
+      List<AttachedFile> attachedFiles = new ArrayList<>();
+
 
       // 각 파트의 데이터를 꺼내 Board 객체에 담는다.
       for (FileItem item : items) {
@@ -67,20 +72,26 @@ public class BoardAddController extends HttpServlet {
           }
 
         } else { // 파일이라면
-          // 다른 클라이언트가 보낸 파일명과 중복되지 않도록 임의의 새 파일명을 생성한다.
+          // 다르클라이언트가 보낸 파일명과 중복되지 않도록 임의의 새 파일명을 생성한다.
           String filename = UUID.randomUUID().toString();
+
+          // 파일 이름을 AttachedFile 객체에 담은 후
+          // DB에 저장할 수 있도록 컬렉션에 저장한다.
+          attachedFiles.add(new AttachedFile(filename));
 
           // 임시 폴더에 저장된 파일을 옮길 폴더 경로 알아내기
           String dirPath = this.getServletContext().getRealPath("/board/files");
           System.out.println(dirPath + "/" + filename);
 
-          // FileItem 객체가 가리키는 임시 폴더에 저장된 파일을 
-          // 우리가 지정한 디렉토리로 옮긴다. 
-          // 이때 파일명은 원래의 이름 대신 UUID로 생성한 이름이다.
+          // FileName 객체가 가리키는 임시 폴더에 저장된 파일을
+          // 우리가 저장한 디렉토리로 옮긴다. 
+          // 이때 파일명은 원래의 이름 대신 UUID로 생성한 이름을 사용한다.
           item.write(new File(dirPath + "/" + filename));
-
         }
       }
+
+      // Board 객체에서 파일명 목록을 담고 있는 컬렉션 객체를 저장한다.
+      board.setAttachedFiles(attachedFiles);
 
       Member loginMember = (Member) request.getSession().getAttribute("loginMember");
       board.setWriter(loginMember);
@@ -93,13 +104,7 @@ public class BoardAddController extends HttpServlet {
 
     } catch (Exception e) {
       request.setAttribute("exception", e);
-      request.getRequestDispatcher("/error.jsp").forward(request, response);
+      request.getRequestDispatcher("/error.jsp").forward(request, response); 
     }
   }
 }
-
-
-
-
-
-
