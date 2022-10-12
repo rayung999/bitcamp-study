b25079d4a347011e62abe7c94effc39ee134faba
rayung999 <kimrawun@naver.com>
2022-10-12 13:49:29 +0900

    '.'

--- board-app/app-server/src/main/java/com/bitcamp/board/listener/ContextLoaderListener.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/listener/ContextLoaderListener.java
@@ -1,5 +1,8 @@
 package com.bitcamp.board.listener;
 
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -7,9 +10,10 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.ServletRegistration.Dynamic;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebListener;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
 import com.bitcamp.board.config.AppConfig;
-import com.bitcamp.servlet.DispatcherServlet;
 
 // 웹애플리케이션이 시작되었을 때 공유할 자원을 준비시키거나 해제하는 일을 한다.
 //
@@ -21,9 +25,11 @@ public class ContextLoaderListener implements ServletContextListener {
     System.out.println("공유 자원을 준비 중!!");
     try {
 
-      // 스프링 IoC 컨테이너 준비
-      AnnotationConfigApplicationContext iocContainer = 
-          new AnnotationConfigApplicationContext(AppConfig.class);
+      // 웹 기능이 포함된 스프링 IoC 컨테이너 준비
+      AnnotationConfigWebApplicationContext iocContainer = 
+          new AnnotationConfigWebApplicationContext();
+      iocContainer.register(AppConfig.class);
+      iocContainer.refresh(); // 자바 config 클래스(AppConfig)의 설정된 대로 객체를 생성한다.
 
       ServletContext ctx = sce.getServletContext();
 
@@ -33,6 +39,17 @@ public class ContextLoaderListener implements ServletContextListener {
       config.addMapping("/service/*");
       config.setMultipartConfig(new MultipartConfigElement(
           this.getClass().getAnnotation(MultipartConfig.class)));
+      config.setLoadOnStartup(1); // 웹 애플리케이션을 시작할 때 프론트 컨트롤러를 자동 생성.
+
+      // 필터 등록
+      CharacterEncodingFilter filter = new CharacterEncodingFilter("UTF-8");
+      FilterRegistration.Dynamic filterConfig = ctx.addFilter("CharacterEncodingFilter", filter);
+      filterConfig.addMappingForServletNames(
+          EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE), 
+          false, 
+          "DispatcherServlet");
+
+
 
     } catch (Exception e) {
       e.printStackTrace();
